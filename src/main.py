cf6a01c763b8882a7373fb2443b2f342254dfc43
n3ph0s <gitlab.gads@simplelogin.co>
2024-08-14 18:06:56 +0800

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, text_type_bold
+
+
+def main():
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
